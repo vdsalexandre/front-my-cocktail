9dde401a8d4cbfd362e771b36ebc6c5c5a927807
vdsalex <alexandre.vandensteen@gmail.com>
2021-01-30 19:24:08 +0100

    adding get request for ingredients and adding ingredient and cocktail class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Ingredient } from './ingredient';
+import { Urls } from './mycocktail-url';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front-my-cocktail';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) { }
+
+  private title: string = 'front-my-cocktail';
+  alcools: Ingredient[];
+  softs: Ingredient[];
+  autres: Ingredient[];
+
+  ngOnInit() {
+    this.findAllIngredientsByType('alcool').subscribe(alcools => { 
+      this.alcools = alcools;
+    });
+    this.findAllIngredientsByType('soft').subscribe(softs => { 
+      this.softs = softs;
+    });
+    this.findAllIngredientsByType('alcool').subscribe(autres => { 
+      this.autres = autres;
+    });
+  }
+
+
+  findAllIngredientsByType(type: string): Observable<Ingredient[]> {
+    return this.http.get<Ingredient[]>(Urls.FIND_ALL_INGREDIENTS_BY_TYPE_URL + type);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,7 +11,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
