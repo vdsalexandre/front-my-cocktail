a1975906ddddccf9884cc7303455cdd21f51b257
vdsalex <alexandre.vandensteen@gmail.com>
2021-01-31 20:35:36 +0100

    adding server status request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { Ingredient } from './ingredient';
 import { Urls } from './mycocktail-url';
 import { catchError, map, tap } from 'rxjs/operators';
+import { Response } from './Response';
 
 @Component({
   selector: 'app-root',
@@ -17,23 +18,34 @@ export class AppComponent implements OnInit {
   alcools: Ingredient[];
   softs: Ingredient[];
   autres: Ingredient[];
+  serverResponse: Response;
 
   ngOnInit() {
-    this.findAllIngredientsByType('alcool').subscribe(alcools => { 
-      this.alcools = alcools;
-    });
-    this.findAllIngredientsByType('soft').subscribe(softs => { 
-      this.softs = softs;
-    });
-    this.findAllIngredientsByType('autre').subscribe(autres => { 
-      this.autres = autres;
-    });
+    this.isServerListening().subscribe(response => {
+      if (response.httpCode === 200) {
+        this.findAllIngredientsByType('alcool').subscribe(alcools => { 
+          this.alcools = alcools;
+        });
+        this.findAllIngredientsByType('soft').subscribe(softs => { 
+          this.softs = softs;
+        });
+        this.findAllIngredientsByType('autre').subscribe(autres => { 
+          this.autres = autres;
+        });
+      }
+    })
   }
 
+  private isServerListening(): Observable<Response> {
+    return this.http.get<Response>(Urls.CHECK_SERVER_STATUS_URL).pipe(
+      tap(response => console.log(`${response.httpCode} - ${response.message}`)),
+      catchError(this.handleError<Response>('isServerListening()'))
+    );
+  }
 
   private findAllIngredientsByType(type: string): Observable<Ingredient[]> {
     return this.http.get<Ingredient[]>(Urls.FIND_ALL_INGREDIENTS_BY_TYPE_URL + type).pipe(
-      tap(_ => console.log(`findAllIngredientsByType: result [ ok ]`)),
+      tap(_ => console.log(`findAllIngredientsByType(${type}): result [ ok ]`)),
       catchError(this.handleError<Ingredient[]>('findAllIngredientsByType', []))
     );
   }
@@ -41,7 +53,7 @@ export class AppComponent implements OnInit {
   private handleError<T>(operation='operation', result?: T) {
     return (error: any): Observable<T> => {
       console.log(error);
-      console.log(`${operation} failed: ${error.message}`);
+      alert(`${operation} failed: ${error.message}`);
       return of(result as T);
     }
   }
