304bc023b42fbcb1a88060aed74bbbdbe6d66a1a
vdsalex <alexandre.vandensteen@gmail.com>
2021-02-05 22:16:54 +0100

    adding search-menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { SearchMenuComponent } from './search-menu/search-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SideMenuComponent
+    SideMenuComponent,
+    SearchMenuComponent
   ],
   imports: [
     BrowserModule,
